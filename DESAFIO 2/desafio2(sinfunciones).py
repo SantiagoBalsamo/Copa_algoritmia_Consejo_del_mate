7f3a538d971088bf0744ee430b43cb30ced3400d
Tonkxt <lisandroacu11@gmail.com>
2026-05-14 15:33:32 -0300

    a

--- DESAFIO 2/desafio2(sinfunciones).py
+++ DESAFIO 2/desafio2(sinfunciones).py
@@ -1,44 +1,9 @@
-#Copa algoritmia 2026 - Desafio 2
-
-# --- Funciones ---
-
-with open('DESAFIO 2/penales.txt', "r") as archivo:
-    lista= archivo.read().strip().upper()
-    
-penales_izquierda=0
-penales_centro=0
-penales_derecha=0
-
-for letra in lista:
-    if letra== "L":
-        penales_izquierda=penales_izquierda+1
-    elif letra== "C":
-        penales_centro=penales_centro+1
-    elif letra== "R":
-        penales_derecha=penales_derecha+1
-
-if penales_izquierda>= penales_centro and penales_izquierda>=penales_derecha:
-    direccion="L"
-    maximo=penales_izquierda
-elif penales_derecha>= penales_centro:
-    direccion="R"
-    maximo=penales_derecha
-else:
-    direccion="C"
-    maximo=penales_centro
-
-print(direccion)
-print(maximo)
-
-#Recibe datos de un archivo .txt y lo valida
-
-#Valida que los caracteres sean correctos (L,R,C) y que no supere la cantidad (<=0 y >=1000)
-
-#Cuenta los caracteres y los ordena de mayor a menor
-
-
-
-
-
-
-# --- Programa inicial --- 
+for caracter in range(1,101):
+    if caracter % 6 == 0:
+         print('fizzbuzz')
+    elif caracter % 2 == 0:
+        print('fizz')
+    elif caracter % 3 == 0:
+        print('buzz')
+    else:
+        print(caracter)
--- DESAFIO 2/desfio2.py
+++ DESAFIO 2/desfio2.py
@@ -1,18 +1,25 @@
-# --- Copa algoritmia 2026 --- 
-# --- Desafio 2 ---
+# --- Copa algoritmia 2026 --- Desafio 2 ---
 
 # --- Funciones ---
 
-# Lee desde un archivo.txt y toma los datos
 def leer_archivo(nombre_archivo):
+    """Abre el archivo y le otorga permisos de lectura.
+    Se lee directamente el contenido en MAYUSCULA, se reemplazan los caracteres indicados y devuelve una cadena vacia.
+
+    Args:
+        Argumento_1(archivo): Nombre del archivo.txt.
+
+    Returns:
+        Retorna el contenido del archivo con las validaciones sobre caracteres dadas.
+
+    Raises:
+        FileNotFoundError: En caso de que el nombre ingresado no coincida con ningun archivo.    
+    """
     with open(nombre_archivo, "r") as archivo:
         contenido = archivo.read().upper()
         contenido = contenido.replace("\n", "").replace("\r", "").replace(" ", "").strip()
     return contenido
 
-# Valida los datos ingresados
-# Los datos no puede superar los 1000 caracteres, y no puede estar vacio
-# Los datos permitidos son: L, R, C.
 def validar_secuencia(secuencia):
     if len(secuencia) == 0:
         return False, "El archivo está vacío."
@@ -25,17 +32,7 @@ def validar_secuencia(secuencia):
 
 # Inicia los contadores y suma cuando encunetra el caracter
 def contar_direcciones(secuencia):
-    cant_L = 0
-    cant_R = 0
-    cant_C = 0
-    for caracter in secuencia:
-        if caracter == "L":
-            cant_L = cant_L + 1
-        elif caracter == "R":
-            cant_R = cant_R + 1
-        elif caracter == "C":
-            cant_C = cant_C + 1
-    return cant_L, cant_R, cant_C
+    return secuencia.count('L'), secuencia.count('R'), secuencia.count('C')
 
 # En caso de empate l>r>c
 def mayor_cantidad(cant_L, cant_R, cant_C):
@@ -61,13 +58,14 @@ def mostrar_resultado(direccion, maximo):
 
 
 # --- PROGRAMA PRINCIPAL ---
-nombre_archivo = "penales"
 
+nombre_archivo = "DESAFIO 2/penales.txt"
 
 # si encuentra un dato no valido, pide que se revise el .txt y toque enter para que lo lea nuevamente
 while True:
     secuencia = leer_archivo(nombre_archivo)
-    es_valida, mensaje = validar_secuencia(secuencia)
+    es_valida = validar_secuencia(secuencia)
+    mensaje = validar_secuencia(secuencia)
     if es_valida:
         cant_L, cant_R, cant_C = contar_direcciones(secuencia)
         direccion, maximo = mayor_cantidad(cant_L, cant_R, cant_C)
@@ -76,6 +74,11 @@ while True:
     else:
         print("Error:", mensaje)
         print("Corregí el archivo penales.txt y presioná ENTER para reintentar...")
-        input()  
-        
+        input()
+
+
+
+
+
+
         
